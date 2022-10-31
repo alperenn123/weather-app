e9c47874cab83bc3d718590c5206d924f5957d66
alperen <alperenn123@gmail.com>
2022-10-31 00:12:53 +0000

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import "./App.css";
 import { ErrorBoundary } from "react-error-boundary";
 import Weather from "./components/weather";
-import { useState } from "react";
 
 function App() {
-  const [shouldRender, setShouldRender] = useState(true);
-
   function MyFallbackComponent({ error, resetErrorBoundary }) {
     return (
       <div role="alert">
@@ -19,12 +16,8 @@ function App() {
     <ErrorBoundary
       FallbackComponent={MyFallbackComponent}
       onError={(error, errorInfo) => console.log({ error, errorInfo })}
-      resetKeys={[shouldRender]}
-      onReset={() => {
-        setShouldRender(false);
-      }}
     >
-      {shouldRender ? <Weather /> : null}
+      <Weather />
     </ErrorBoundary>
   );
 }
